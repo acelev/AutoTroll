6322afcf1ad853bc77c4b3ae1946b82c561ad98e
Ace Levenberg <acelevenberg@gmail.com>
2014-04-29 22:53:24 -0700

    implemented some of AutoTrollThread and added more tests

--- src/AutoTrollThread.py
+++ src/AutoTrollThread.py
@@ -4,55 +4,18 @@
 #"THE BEER-WARE LICENSE" (Revision 42):
 #<acelevenberg@gmail.com> wrote this file. As long as you retain this notice you
 #can do whatever you want with this stuff. If we meet some day, and you think
-#this stuff is worth it, you an buy me a beer in return Ace Levenberg
+#this stuff is worth it, you can buy me a beer in return
 #----------------------------------------------------------------------------
 
 import threading
 from Queue import Queue
 import praw
 
-class praw_submission_wrapper():
-    """
-    wrapper to wrap submissions and comment objects together
-    access to original object is available because im lazy
-    usage:
-        submission = praw_submissions_wrapper(comment)
-        submission.respond("Spam Eggs")
-        submissions.down_vote() 
-    """ 
-    UP = 0
-    DOWN = 1
-    def __init__(self,praw_content_object):
-        """
-        creates a comment object
-        params:
-            praw_content_object must be a submissions or comment object
-        """
-        pass
-    
-    def respond(self, comment):
-        """
-        responds to the submission
-        params:
-            comment(string) the response to the submissions 
-        returns:
-            a praw_submission_wrapper object containing the comment object returned
-            by PRAW  
-        """
-        pass
-    def vote(self, up_or_down):
-        """ 
-        votes up or down
-        params:
-            up_or_down(int) either UP or DOWN
-        """
-        pass 
-
 class AutoTrollThread(threading.Thread):
 
     """
     The actual AutoTroll
-    each AutoTrollThread has its own troll login and 
+    each AutoTrollThread has its own troll login and
     praw reddit object
     usage:
     auto_troll = AutoTrollThread('username', 'password')
@@ -63,42 +26,83 @@ class AutoTrollThread(threading.Thread):
         """
         creates an instance of AutoTrollThread
         throws (login failure) if the login fails
-        
+
         """
         self.input_queue = Queue()
+
+        self.reddit = praw.Reddit(user_agent='AutoTroll v0.1')
+        self.reddit.login(username, password)
 
     def send(self, comment_to_troll):
         """
         inserts a post/comment PRAW object into the thread queue
         params:
-            comment_to_troll(praw_submissions_wrapper) the comment for this instance to troll 
+            comment_to_troll(PRAW POST or COMMENT) the comment for this instance to troll
         """
-        self.input_queue.put(comment_to_troll) 
+        self.input_queue.put(comment_to_troll)
     def close(self):
-        """ 
+        """
         closes the thread
         """
         self.input_queue.put(None)
-        self.input_queue.join() 
+        self.input_queue.join()
     def run(self):
         """
         starts the thread
         """
-        pass 
+        while True:
+            submisson = self.input_queue.get()
+            if submisson is None:
+                break
+            insult = self.get_insult()
+            self.post(insult, submission)
+            self.input_queue.task_done()
+        self.input_queue.task_done()
+        return
+
     def get_insult(self):
         """
         gets insult to troll chump with
+        Insultgenerator.org is awesome!
         returns:
             (string) the insult
-        """   
-        pass
+        """
+        #"parses" the html, I need to find one tag that is at the top level
+        #this is just how insultgenerator works I know its dirty
+        response = urllib2.urlopen('http://insultgenerator.org')
+        insult = ""
+        for line in response:
+            match_object = re.match(r'<TR align=center><TD>(.*)', line)
+            if match_object is not None:
+                insult = match_object.group(1)
+                break
+        response.close()
+        return insult
+
     def post(self, trolled_response, comment_to_troll):
         """
         posts a trolled_response to the comment provided
         params:
             trolled_response(string) the insult to respond
-            comment_to_troll(PRAW comment/post object) to post to respond to 
+            comment_to_troll(PRAW post) to post to respond to
         returns:
-            praw wrapped comment
+            the comment object
         """
-        pass
+        reply_func = None
+        if isinstance(comment_to_troll, praw.objects.Comment):
+            reply_func = comment_to_troll.reply
+        elif isinstance(comment_to_troll, praw.objects.Submission):
+            reply_func = comment_to_troll.add_comment
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/AutoTrollThread_test.py
+++ test/AutoTrollThread_test.py
@@ -1,14 +1,25 @@
+#Auto Troll
+#Author: Ace Levenberg (acelevenberg@gmail.com)
+#----------------------------------------------------------------------------
+#"THE BEER-WARE LICENSE" (Revision 42):
+#<acelevenberg@gmail.com> wrote this file. As long as you retain this notice you
+#can do whatever you want with this stuff. If we meet some day, and you think
+#this stuff is worth it, you can buy me a beer in return
+#----------------------------------------------------------------------------
+
 import unittest
 import praw
 import sys
 import requests
 
-from AutoTrollThread import AutoTrollThread, praw_submission_wrapper
+from AutoTrollThread import AutoTrollThread
+
 
 class TestAutoTrollThread(unittest.TestCase):
+
     def setUp(self):
         self.reddit = praw.Reddit(user_agent='Auto Troll Testing v 0.1')
-        self.login, self.password = self.get_login() 
+        self.login, self.password = self.get_login()
         self.troll = AutoTrollThread(self.login, self.password)
 
     def test_AutoTrollCreation(self):
@@ -16,29 +27,53 @@ class TestAutoTrollThread(unittest.TestCase):
         self.assertIsInstance(troll, AutoTrollThread)
 
     def test_AutoTrollBadCreation(self):
-        self.assertRaises(praw.errors.InvalidUserPass, AutoTrollThread, self.login, 'notpassword' )
+        self.assertRaises(praw.errors.InvalidUserPass,
+                          AutoTrollThread,
+                          self.login,
+                          'notpassword')
+
     def test_AutoTrollSend(self):
-        thing = object
+        thing = {}
         self.troll.send(thing)
         self.assertIs(thing, self.troll.input_queue.get())
-    def test_AutoTrollClose(self):
-        self.troll.start()
-        self.troll.close()
-        self.assertIs(None, self.troll.input_queue.get()) 
-    
-    def test_AuoTrollPost(self):
+
+    def test_AutoTrollPost(self):
         botcirclejerk = self.reddit.get_subreddit('botcirclejerk')
         random = botcirclejerk.get_random_submission()
-        random_wrapped =  praw_submission_wrapper(random)
         try:
-            comment = self.troll.post('foo bar baz qux', random_wrapped)
-        except request.exceptions.HTTPError as e:
+            #if a comment is returned I know it was posted due to PRAW
+            comment = self.troll.post('foo bar baz qux', random)
+            self.assertIsInstance(comment, praw.objects.Comment)
+        except requests.exceptions.HTTPError as e:
             self.fail(msg=e.toString())
+
+    def test_AutoTrollPostNotCommentOrSubmission(self):
+        self.troll.post("foo bar", {})
+        #not sure how this is suppose to respond yet
+
+
+    def test_AutoTrollPostTwice(self):
+        #unsure of proper response at the moment
+        botcirclejerk =  self.reddit.get_subreddit('botcirclejerk')
+        random = botcirclejerk.get_random_submission()
+        try:
+            self.troll.post('spam eggs', random)
+        except requests.exceptions.HTTPError as e:
+            self.fail(msg=e.toString())
+
+    def test_getInsult(self):
+        try:
+            insult = self.troll.get_insult()
+        except requests.exceptions.HTTPError as e:
+            self.fail(msg=e.toString())
+        self.assertIsInstance(insult, str)
+        self.assertGreater(len(insult), 1)
+
     def get_login(self):
-        with open('../src/passwords.txt') as password: 
+        with open('../src/passwords.txt') as password:
             line = password.readline().split('=')
         login = line[0]
-        password = line[1]  
+        password = line[1]
         return login, password
 
 if __name__ == '__main__':
