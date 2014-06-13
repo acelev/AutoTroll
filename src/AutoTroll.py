e14ed3fb5d0638d5dd6fd07e3f22bfa0b4ac6cc7
Ace Levenberg <acelevenberg@gmail.com>
2014-06-12 21:00:08 -0700

    moved autotrollmanager to its own file

--- src/AutoTroll.py
+++ src/AutoTroll.py
@@ -1,7 +1,6 @@
 import sys
 import praw
 from CommentStore import comment_store
-from AutoTrollThread import AutoTrollThread
 
 
 def main(*args, **kwargs):
@@ -84,72 +83,6 @@ class ChumpManager():
         for chump in self.comment_store.get_stored_chumps():
             self.add_chump(chump)
 
-
-class AutoTrollManager():
-
-    def __init__(self):
-        self.trolls = dict()
-        self._removed_trolls = dict()
-
-    def remove_troll(self, username):
-        """
-        removes a troll from the army of trolls
-        params:
-            username, the name of the troll to remove
-        """
-        # Move the AutoTrollThread to a different list
-        # close the thread
-        # there needs to be a thread clean up that deletes the objects
-        # once their queues are empty
-        # it could aslo steal the submissions from the threads and then
-        # remove them from the threads, and insert them elsewhere (probably a
-        # better option)
-        pass
-
-    def add_troll(self, username, password):
-        """
-        Adds a new troll to the army of trolls
-        params:
-            username, the username name of the troll
-            password, the password ofer the trol
-        """
-        try:
-            self.trolls[username] = AutoTrollThread(username, password)
-        except praw.errors.APIException as e:
-            # log error
-            print str(e)
-
-    def add_trolls(self, logins):
-        """
-        adds n trolls
-        params:
-            logins, a list of tuples containing username, password pairs
-        """
-        for login, password in logins:
-            self.add_troll(login, password)
-
-    def _create_auto_trolls(self, login_info):
-        """
-        creates n auto trolls
-        this will take at least 2 * n seconds because reddit only allows one
-        request every two seconds
-        params:
-            login_info, a list of username, password tuples
-        returns:
-            a list of AutoTrollThreds
-        """
-        # this fuinction is probably going to get removed
-        return [troll for troll in map(
-                                    lambda user, password: AutoTrollThread(user, password),
-                                    login_info)]
-
-    def send_submission(self, submission):
-        """
-        sends a submission to the Troll with the least load on it
-        input:
-            submission(praw submission object) submission to troll
-        """
-        pass
 
 def read_passwords_file(password_file):
     """
